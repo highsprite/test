7c6099c9f8eed47d62a4806bfa14a87b902bd002
yinjie wang <yinjiewang@yinjietekiMacBook-Air.local>
2014-05-31 15:33:46 +0200

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by yinjie wang on 14-5-31.
+//  Copyright (c) 2014年 yinjie wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
